710481ff88b35962adb12ce7531632b9e3c3ae01
Daniel <dkilcullen25@gmail.com>
2021-12-13 18:33:08 -0600

    commit tree b4e6359a9be9b98b6eafdf4fce214191b5762c51

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(destination - start)) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const feet = distanceTravelledInFeet(start, destination)
+    if ( feet > 2500 ) {
+        return 'cannot travel that far';
+    } else if ( feet >  2000 ) {
+        return 25;
+    } else if ( feet > 400 ) {
+        return 0.02 * ( feet - 400 );
+    } else {
+        return 0;
+    }
+}
